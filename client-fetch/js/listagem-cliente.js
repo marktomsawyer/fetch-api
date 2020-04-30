8820c12d2a089ff5f08432f36c0623a99f99f418
user.name "Mark Tom Sawyer <user.email "mark@tomsawyer.com.br>
2020-04-30 12:26:41 -0300

    Versao HTML-JS

---
+++ client-fetch/js/listagem-cliente.js
@@ -0,0 +1,34 @@
+const exibeCliente = (cpf, nome, id) => {
+  const linha = document.createElement("tr");
+
+  const conteudoLinha = `
+  <td>${cpf}</td>
+  <td>${nome}</td>
+  <button type="button" class="btn btn-danger"
+  onclick="removeCliente(${id})">Excluir</button>
+  <a href="edita-clientes.html?id=${id}">
+    <button type="button" class="btn btn-info">Editar</button>
+  </a>
+    `;
+
+  linha.innerHTML = conteudoLinha;
+
+  return linha;
+};
+
+const removeCliente = id => {
+  if (confirm("Deseja deletar o cliente?")) {
+    //debugger;
+    deletaCliente(id)
+    document.location.reload();
+    window.location.reload();
+  }
+};
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+
+listarClientes().then(exibe => {
+  exibe.forEach(indice => {
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id));
+  });
+});
